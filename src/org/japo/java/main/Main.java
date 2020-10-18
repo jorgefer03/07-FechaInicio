cfd1b74413796e24adeb646d56c2ac893bc85607
jorgefer03 <72352401+jorgefer03@users.noreply.github.com>
2020-10-18 20:18:40 +0200

    Commit inicial :tada:

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Jorge Fernández Majid - jorge.fernandez.alum@iescamp.es
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
